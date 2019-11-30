287b32196ffea09613be76dab181bb82af8adbc9
muhzi <muhsinkeramam@gmail.com>
2019-11-30 15:35:28 +0530

    add theme to app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import { PersistGate } from 'redux-persist/integration/react';
+import { mapping } from '@eva-design/eva';
+import { ApplicationProvider } from 'react-native-ui-kitten';
+
+import { theme } from './theme';
 
 import Router from './router';
 import { store, persistor } from './store';
@@ -9,11 +13,13 @@ import { store, persistor } from './store';
 export default class Chatwoot extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Router />
-        </PersistGate>
-      </Provider>
+      <ApplicationProvider mapping={mapping} theme={theme}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Router />
+          </PersistGate>
+        </Provider>
+      </ApplicationProvider>
     );
   }
 }

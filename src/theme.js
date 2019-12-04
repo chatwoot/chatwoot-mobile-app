ea5b4c7a78dbcfb68ac5ad34c23bb12cea98fc9a
muhzi <muhsinkeramam@gmail.com>
2019-12-04 16:08:17 +0530

    update status bar color

--- src/theme.js
+++ src/theme.js
@@ -3,4 +3,7 @@ import { light as lightTheme } from '@eva-design/eva';
 
 export const theme = {
   ...lightTheme,
+
+  'background-primary-color-1': '#1f93ff',
+  'border-primary-color-1': '#1f93ff',
 };

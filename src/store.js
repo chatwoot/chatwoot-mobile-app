eb51815dff6f230c288ad6b2106cd394a460829f
muhzi <muhsinkeramam@gmail.com>
2019-12-24 16:07:28 +0530

    add redux-thunk middleware

--- src/store.js
+++ src/store.js
@@ -2,20 +2,21 @@ import AsyncStorage from '@react-native-community/async-storage';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 
-import rootReducer from './reducer';
-// Middleware: Redux Persist Config
+import rootReducer from './reducer'; // the value from combineReducers
+
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  whitelist: [],
-  blacklist: [],
 };
-// Middleware: Redux Persist Persisted Reducer
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-// Redux: Store
-const store = createStore(persistedReducer, applyMiddleware(createLogger()));
-// Middleware: Redux Persist Persister
-let persistor = persistStore(store);
+
+const store = createStore(
+  persistedReducer,
+  applyMiddleware(createLogger(), thunk),
+);
+const persistor = persistStore(store);
 
 export { store, persistor };

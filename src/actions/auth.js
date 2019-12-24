981524c3de82e84d6e899554d7207f219ab6817f
muhzi <muhsinkeramam@gmail.com>
2019-12-24 18:55:43 +0530

    remove console.log

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -14,7 +14,7 @@ export const onLogin = ({ email, password }) => async dispatch => {
     const { data } = response.data;
     dispatch({ type: LOGIN_SUCCESS, payload: data });
   } catch (error) {
-    console.log('error', error);
+    console.log('error->', error);
     dispatch({ type: LOGIN_ERROR, payload: error });
   }
 };

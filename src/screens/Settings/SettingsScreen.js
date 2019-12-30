0e8bc2454cc3865becc7523228e75a8a7ce503a2
muhzi <muhsinkeramam@gmail.com>
2019-12-30 16:29:10 +0530

    add filter screen in navigation screen

--- src/screens/Settings/SettingsScreen.js
+++ src/screens/Settings/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, TopNavigation } from 'react-native-ui-kitten';
+import { TopNavigation } from 'react-native-ui-kitten';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { SafeAreaView } from 'react-native';

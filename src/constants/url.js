5e4acf3611fc5d7b1d4aa3133d59e95c04a1b49d
muhzi <muhsinkeramam@gmail.com>
2020-01-04 16:37:20 +0530

    add websocket url

--- src/constants/url.js
+++ src/constants/url.js
@@ -2,4 +2,6 @@ export const BASE_URL = 'https://chatwoot.herokuapp.com/';
 
 export const API = 'api/v1/';
 
+export const WEB_SOCKET_URL = 'wss://chatwoot.herokuapp.com/cable';
+
 export const GRAVATAR_URL = 'https://www.gravatar.com/avatar/';

4e012737dcb3bf0b9351fa18af5ee6c341f29a63
muhzi <muhsinkeramam@gmail.com>
2020-01-16 22:46:26 +0530

    handle back button action

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { Alert, BackHandler } from 'react-native';
 
 import { PersistGate } from 'redux-persist/integration/react';
 import { mapping } from '@eva-design/eva';
@@ -11,7 +12,40 @@ import { theme } from './theme';
 import Router from './router';
 import { store, persistor } from './store';
 
+import i18n from './i18n';
+
 export default class Chatwoot extends Component {
+  componentDidMount() {
+    BackHandler.addEventListener(
+      'hardwareBackPress',
+      this.handleBackButtonClick,
+    );
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener(
+      'hardwareBackPress',
+      this.handleBackButtonClick,
+    );
+  }
+
+  handleBackButtonClick = () => {
+    Alert.alert(
+      i18n.t('EXIT.TITLE'),
+      i18n.t('EXIT.SUBTITLE'),
+      [
+        {
+          text: i18n.t('EXIT.CANCEL'),
+          onPress: () => {},
+          style: 'cancel',
+        },
+        { text: i18n.t('EXIT.OK'), onPress: () => BackHandler.exitApp() },
+      ],
+      { cancelable: false },
+    );
+    return true;
+  };
+
   render() {
     return (
       <React.Fragment>

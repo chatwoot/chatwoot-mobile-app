c7660e2bcf128238ea521d1f908788a3915c7372
muhzi <muhsinkeramam@gmail.com>
2020-01-20 21:42:45 +0530

    Add all in inbox filter

--- src/actions/inbox.js
+++ src/actions/inbox.js
@@ -9,16 +9,19 @@ import axios from '../helpers/APIHelper';
 
 import { API } from '../constants/url';
 
+const defaultInbox = { id: 0, name: 'All' };
 export const getInboxes = () => async dispatch => {
   dispatch({ type: GET_INBOX });
   try {
     const response = await axios.get(`${API}inboxes.json`);
-    const { payload } = response.data;
+    let { payload } = response.data;
 
+    payload = [defaultInbox, ...payload];
     dispatch({
       type: GET_INBOX_SUCCESS,
       payload: payload,
     });
+    dispatch(setInbox({ inbox: defaultInbox }));
   } catch (error) {
     dispatch({ type: GET_INBOX_ERROR, payload: error });
   }

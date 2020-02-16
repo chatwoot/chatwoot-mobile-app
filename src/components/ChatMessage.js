adb5551d9f82877c78cb699c6398e01cfe53e4c7
muhzi <muhsinkeramam@gmail.com>
2020-02-16 15:34:34 +0800

    Add activity message component in chat screen

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,13 +1,57 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 
 import { View, StyleSheet, Dimensions } from 'react-native';
 
 import PropTypes from 'prop-types';
 
+import { Icon } from 'react-native-ui-kitten';
+
 import CustomText from './Text';
 
 import { theme } from '../theme';
 import { messageStamp } from '../helpers/TimeHelper';
+
+const PersonIcon = style => {
+  return <Icon {...style} name="person-outline" />;
+};
+
+const IncomingMessage = ({ message, created_at }) => (
+  <React.Fragment>
+    <View style={styles.messageLeft}>
+      <CustomText style={styles.messageContent}>{message.content}</CustomText>
+    </View>
+    <CustomText style={styles.date}>
+      {messageStamp({ time: created_at })}
+    </CustomText>
+  </React.Fragment>
+);
+
+const OutGoingMessage = ({ message, created_at }) => (
+  <React.Fragment>
+    <CustomText style={styles.date}>
+      {messageStamp({ time: created_at })}
+    </CustomText>
+
+    <View style={styles.messageRight}>
+      <CustomText style={styles.messageContent}>{message.content}</CustomText>
+    </View>
+  </React.Fragment>
+);
+
+const ActivityMessage = ({ message, created_at }) => (
+  <View style={styles.activityView}>
+    <View style={styles.activityDateView}>
+      <CustomText style={styles.date}>
+        {messageStamp({ time: created_at })}
+      </CustomText>
+    </View>
+    <View style={styles.messageActivity}>
+      <PersonIcon style={styles.icon} />
+      <CustomText style={styles.messageContent}>{message.content}</CustomText>
+    </View>
+  </View>
+);
 
 const propTypes = {
   message: PropTypes.shape({
@@ -25,33 +69,21 @@ class ChatMessage extends Component {
     const { message } = this.props;
 
     const { message_type, created_at } = message;
-    const alignment = message_type ? 'flex-end' : 'flex-start';
+
+    let alignment = message_type ? 'flex-end' : 'flex-start';
+    if (message_type === 2) {
+      alignment = 'center';
+    }
 
     return (
       <View style={[styles.message, { justifyContent: alignment }]}>
         <View style={styles.messageContainer}>
           {alignment === 'flex-start' ? (
-            <React.Fragment>
-              <View style={styles.messageLeft}>
-                <CustomText style={styles.messageContent}>
-                  {message.content}
-                </CustomText>
-              </View>
-              <CustomText style={styles.date}>
-                {messageStamp({ time: created_at })}
-              </CustomText>
-            </React.Fragment>
+            <IncomingMessage message={message} created_at={created_at} />
+          ) : alignment === 'center' ? (
+            <ActivityMessage message={message} created_at={created_at} />
           ) : (
-            <React.Fragment>
-              <CustomText style={styles.date}>
-                {messageStamp({ time: created_at })}
-              </CustomText>
-              <View style={styles.messageRight}>
-                <CustomText style={styles.messageContent}>
-                  {message.content}
-                </CustomText>
-              </View>
-            </React.Fragment>
+            <OutGoingMessage message={message} created_at={created_at} />
           )}
         </View>
       </View>
@@ -95,6 +127,26 @@ const styles = StyleSheet.create({
     left: 4,
     backgroundColor: theme['color-background-message'],
     marginLeft: 16,
+  },
+
+  activityView: {
+    padding: 8,
+    borderRadius: 8,
+    maxWidth: Dimensions.get('window').width - 50,
+    backgroundColor: theme['color-background-activity'],
+  },
+  activityDateView: {
+    alignItems: 'flex-end',
+  },
+
+  messageActivity: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 16,
+    height: 16,
   },
 
   messageContent: {

62f81163bc8f4e057a8eeac5685161c10649d839
muhzi <muhsinkeramam@gmail.com>
2020-03-26 16:48:47 +0530

    Add color for image loader

--- src/components/ImageLoader.js
+++ src/components/ImageLoader.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { View, ActivityIndicator } from 'react-native';
 import { withStyles } from '@ui-kitten/components';
+import { theme } from '../theme';
 
 const ImageLoader = ({ style }) => {
   return (
     <View style={style}>
-      <ActivityIndicator size="small" />
+      <ActivityIndicator size="small" color={theme['loader-color']} />
     </View>
   );
 };

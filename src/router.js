9d7edf191315ab8821782fdd60dd330a1e1279c6
Muhsin K <muhsinkeramam@gmail.com>
2020-04-07 19:55:44 +0530

    Add ios changes for deep linking

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+
+import { useSelector } from 'react-redux';
+import { Linking } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import PropTypes from 'prop-types';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 import TabBar from './components/TabBar';
@@ -30,62 +32,67 @@ const SettingsStack = () => (
 );
 
 const TabStack = () => (
-  <Tab.Navigator tabBar={props => <TabBar {...props} />}>
+  <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
     <Tab.Screen name="Home" component={HomeStack} />
     <Tab.Screen name="Settings" component={SettingsStack} />
   </Tab.Navigator>
 );
 
-class RootApp extends Component {
-  static propTypes = {
-    isLogged: PropTypes.bool,
-  };
+const useMount = (func) => useEffect(() => func(), []);
 
-  static defaultProps = {
-    isLogged: false,
-  };
-  render() {
-    const { isLogged } = this.props;
+const useInitialURL = () => {
+  const [url, setUrl] = useState(null);
+  const [processing, setProcessing] = useState(true);
 
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login" headerMode={'none'}>
-          {isLogged ? (
-            <>
-              <Stack.Screen name="Tab" component={TabStack} />
-              <Stack.Screen name="ChatScreen" component={ChatScreen} />
-              <Stack.Screen
-                name="ConversationFilter"
-                component={ConversationFilter}
-              />
-              <Stack.Screen name="ImageScreen" component={ImageScreen} />
-            </>
-          ) : (
-            <>
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="ResetPassword" component={ResetPassword} />
-              <Stack.Screen
-                name="ConversationList"
-                component={ConversationList}
-              />
+  useMount(() => {
+    const getUrlAsync = async () => {
+      // Get the deep link used to open the app
+      const initialUrl = await Linking.getInitialURL();
 
-              <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
-            </>
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
+      // The setTimeout is just for testing purpose
+      setTimeout(() => {
+        setUrl(initialUrl);
+        setProcessing(false);
+      }, 1000);
+    };
+
+    getUrlAsync();
+  });
+
+  return { url, processing };
+};
+
+export default function RootApp() {
+  const isLogged = useSelector((state) => state.auth.isLogged);
+  const { url: initialUrl, processing } = useInitialURL();
+  console.log('initialUrl', initialUrl, processing);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" headerMode={'none'}>
+        {isLogged ? (
+          <>
+            <Stack.Screen name="Tab" component={TabStack} />
+            <Stack.Screen name="ChatScreen" component={ChatScreen} />
+            <Stack.Screen
+              name="ConversationFilter"
+              component={ConversationFilter}
+            />
+            <Stack.Screen name="ImageScreen" component={ImageScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="ResetPassword" component={ResetPassword} />
+            <Stack.Screen
+              name="ConversationList"
+              component={ConversationList}
+            />
+
+            <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-function bindAction() {
-  return {};
-}
-
-function mapStateToProps(state) {
-  return {
-    isLogged: state.auth.isLogged,
-  };
-}
-
-export default connect(mapStateToProps, bindAction)(RootApp);

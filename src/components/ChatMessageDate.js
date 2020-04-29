51209f4d57818041c674a437ae4ef1dbd0fe36bf
Muhsin K <muhsinkeramam@gmail.com>
2020-04-29 18:40:17 +0530

    Create component from displaying date header in conversation

---
+++ src/components/ChatMessageDate.js
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { View } from 'react-native';
+import { withStyles } from '@ui-kitten/components';
+import PropTypes from 'prop-types';
+import CustomText from './Text';
+
+const styles = (theme) => ({
+  mainView: {
+    flex: 1,
+    flexDirection: 'row',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dateView: {
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: theme['color-background-date'],
+    elevation: 1,
+  },
+  text: {
+    fontSize: theme['font-size-extra-small'],
+  },
+});
+
+const ChatMessageDateComponent = ({ date, themedStyle }) => {
+  return (
+    <View style={themedStyle.mainView}>
+      <View style={themedStyle.dateView}>
+        <CustomText style={themedStyle.text}>{date}</CustomText>
+      </View>
+    </View>
+  );
+};
+
+const propTypes = {
+  date: PropTypes.string,
+};
+
+const defaultProps = {
+  date: null,
+};
+
+const ChatMessageDate = withStyles(ChatMessageDateComponent, styles);
+
+ChatMessageDate.defaultProps = defaultProps;
+ChatMessageDate.propTypes = propTypes;
+
+export default ChatMessageDate;

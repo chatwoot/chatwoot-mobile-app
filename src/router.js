65f8ccd9a1956ff4afb09f060f51cdce64e20186
Muhsin K <muhsinkeramam@gmail.com>
2020-05-03 20:37:47 +0530

    Add custom hook for finding the deep url

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -53,11 +53,37 @@ const defaultProps = {
   isUrlSet: false,
 };
 
-const useInitialURL = async () => {
-  const initialUrl = await Linking.getInitialURL();
-  if (initialUrl) {
-    doDeepLinking({ url: initialUrl });
-  }
+const useDeepLinkURL = () => {
+  const [linkedURL, setLinkedURL] = useState(null);
+
+  // 1. If the app is not already open, it is opened and the url is passed in as the initialURL
+  // You can handle these events with Linking.getInitialURL(url) -- it returns a Promise that
+  // resolves to the url, if there is one.
+  // Get the deep link used to open the app in minimized state
+  useEffect(() => {
+    const getUrlAsync = async () => {
+      // Get the deep link used to open the app
+      const initialUrl = await Linking.getInitialURL();
+      setLinkedURL(decodeURI(initialUrl));
+    };
+
+    getUrlAsync();
+  }, []);
+
+  // 2. If the app is already open, the app is foregrounded and a Linking event is fired
+  // You can handle these events with Linking.addEventListener(url, callback)
+  // Get the deep link used to open the app in closed state
+  useEffect(() => {
+    const callback = ({ url }) => setLinkedURL(decodeURI(url));
+    Linking.addEventListener('url', callback);
+    return () => {
+      Linking.removeEventListener('url', callback);
+    };
+  }, []);
+
+  const resetURL = () => setLinkedURL(null);
+
+  return { linkedURL, resetURL };
 };
 
 const _handleOpenURL = (event) => {
@@ -68,17 +94,16 @@ const App = () => {
   const isLogged = useSelector((state) => state.auth.isLogged);
   const isUrlSet = useSelector((state) => state.settings.isUrlSet);
   const locale = useSelector((state) => state.settings.localeValue);
+  const { linkedURL, resetURL } = useDeepLinkURL();
+
+  useEffect(() => {
+    resetURL();
+  }, [linkedURL, resetURL]);
+  if (linkedURL) {
+    _handleOpenURL({ url: linkedURL });
+  }
 
   i18n.locale = locale;
-  // Get the deep link used to open the app in minimized state
-  useEffect(() => {
-    Linking.addEventListener('url', _handleOpenURL);
-    return () => {
-      Linking.removeEventListener('url');
-    };
-  }, []);
-  // Get the deep link used to open the app in closed state
-  useInitialURL();
 
   return (
     <NavigationContainer ref={navigationRef}>

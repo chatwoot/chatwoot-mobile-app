fa2913071c4990ea1ecc24516d688e4f9dd61fe9
Muhsin K <muhsinkeramam@gmail.com>
2020-05-05 09:31:25 +0530

    Enable sentry in app

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import { AppRegistry } from 'react-native';
+import * as Sentry from '@sentry/react-native';
 
 import { name as appName } from './app.json';
+
+import { SENTRY_TOKEN_URL } from './sentry';
 
 import App from './src/app';
 
 AppRegistry.registerComponent(appName, () => App);
+
+Sentry.init({
+  dsn: SENTRY_TOKEN_URL,
+});

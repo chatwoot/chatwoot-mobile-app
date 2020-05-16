8cf5ba95ecd15ed0512a88cc253d960711f718fb
Muhsin K <muhsinkeramam@gmail.com>
2020-05-16 13:03:24 +0530

    Add custom icon component

---
+++ src/components/Icon.js
@@ -0,0 +1,26 @@
+import { Icon } from '@ui-kitten/components';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { theme } from '../theme';
+
+const CustomIcon = ({ name, color, height, width, ...customProps }) => {
+  return <Icon height={height} width={width} fill={color} name={name} {...customProps} />;
+};
+
+const propTypes = {
+  color: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  height: PropTypes.number,
+  width: PropTypes.number,
+};
+
+const defaultProps = {
+  color: theme['text-light-color'],
+  height: 24,
+  width: 24,
+};
+
+CustomIcon.propTypes = propTypes;
+CustomIcon.defaultProps = defaultProps;
+
+export default CustomIcon;

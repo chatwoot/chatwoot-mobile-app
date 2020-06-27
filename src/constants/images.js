560788b0d91565595397c0b430534b4423a5f0f8
Muhsin K <muhsinkeramam@gmail.com>
2020-06-27 22:46:10 +0530

    Add channel badge in user avatar

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,9 @@
 const appLogo = require('../assets/images/appLogo.png');
+const twitterBadge = require('../assets/images/twitter-badge.png');
+const fbBadge = require('../assets/images/fb-badge.png');
 
 export default {
   appLogo,
+  twitterBadge,
+  fbBadge,
 };

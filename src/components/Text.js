71f212988d60ad76cb1ab160d4bbb79009bb31a9
Muhsin K <muhsinkeramam@gmail.com>
2020-07-05 12:47:35 +0530

    Fix prop type warnings

--- src/components/Text.js
+++ src/components/Text.js
@@ -18,7 +18,6 @@ class CustomText extends Component {
     // style: PropTypes.arrayOf(PropTypes.object),
     weight: PropTypes.oneOf(['300', '400', '500', '600', '700', '800', '900']),
     locale: PropTypes.string,
-    children: PropTypes.string,
   };
 
   findWeightVariant = (props) => {

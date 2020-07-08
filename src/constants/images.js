10e31fd49493dd5cde14cb9c9a0ca124bc50fd74
Muhsin K <muhsinkeramam@gmail.com>
2020-07-08 19:59:51 +0530

    Add path for all the images

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,10 +2,20 @@ const appLogo = require('../assets/images/appLogo.png');
 const twitterBadge = require('../assets/images/twitter-badge.png');
 const fbBadge = require('../assets/images/fb-badge.png');
 const whatsAppBadge = require('../assets/images/whatsApp-badge.png');
+const URL = require('../assets/images/url.png');
+const login = require('../assets/images/login.png');
+const emptyNotifications = require('../assets/images/emptyNotifications.png');
+const forgotPassword = require('../assets/images/forgotPassword.png');
+const emptyConversations = require('../assets/images/emptyConversations.png');
 
 export default {
   appLogo,
   twitterBadge,
   fbBadge,
   whatsAppBadge,
+  URL,
+  login,
+  emptyNotifications,
+  forgotPassword,
+  emptyConversations,
 };

27a512557e36178b2ce51a873f633d5e95c53087
Muhsin Keloth <muhsinkeramam@gmail.com>
2020-07-19 22:47:16 +0530

    Compress all the images (#213)

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,10 +2,10 @@ const appLogo = require('../assets/images/appLogo.png');
 const twitterBadge = require('../assets/images/twitter-badge.png');
 const fbBadge = require('../assets/images/fb-badge.png');
 const whatsAppBadge = require('../assets/images/whatsApp-badge.png');
-const URL = require('../assets/images/url.png');
-const login = require('../assets/images/login.png');
-const emptyNotifications = require('../assets/images/emptyNotifications.png');
-const forgotPassword = require('../assets/images/forgotPassword.png');
+const URL = require('../assets/images/url.jpg');
+const login = require('../assets/images/login.jpg');
+const emptyNotifications = require('../assets/images/emptyNotifications.jpg');
+const forgotPassword = require('../assets/images/forgotPassword.jpg');
 const emptyConversations = require('../assets/images/emptyConversations.png');
 
 export default {

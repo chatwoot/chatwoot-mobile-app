5648d8a5932eac64ad7ec0f1095add6eaf2f1fbe
Muhsin K <muhsinkeramam@gmail.com>
2020-08-28 23:30:45 +0530

    Disable sentry now

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { AppRegistry } from 'react-native';
-import * as Sentry from '@sentry/react-native';
+// import * as Sentry from '@sentry/react-native';
 
 import { name as appName } from './app.json';
 
-import { SENTRY_TOKEN_URL } from './sentry';
+// import { SENTRY_TOKEN_URL } from './sentry';
 
 import App from './src/app';
-
+// import App from './App';
 AppRegistry.registerComponent(appName, () => App);
 
-Sentry.init({
-  dsn: SENTRY_TOKEN_URL,
-});
+// Sentry.init({
+//   dsn: SENTRY_TOKEN_URL,
+// });

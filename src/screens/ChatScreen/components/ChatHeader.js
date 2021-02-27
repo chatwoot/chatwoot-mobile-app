7f15cb7b3fb44f61a5a214b51b98a086b067c675
Muhsin <muhsinkeramam@gmail.com>
2021-02-27 13:00:54 +0530

    code cleanup

--- src/screens/ChatScreen/components/ChatHeader.js
+++ src/screens/ChatScreen/components/ChatHeader.js
@@ -12,14 +12,6 @@ import CustomText from '../../../components/Text';
 import { toggleConversationStatus } from '../../../actions/conversation';
 
 const styles = (theme) => ({
-  mainContainer: {
-    flex: 1,
-    backgroundColor: theme['background-basic-color-1'],
-  },
-
-  keyboardView: {
-    flex: 1,
-  },
   headerView: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -40,58 +32,6 @@ const styles = (theme) => ({
     color: theme['color-gray'],
     paddingTop: 4,
     paddingLeft: 4,
-  },
-
-  container: {
-    flex: 1,
-    backgroundColor: theme['color-background'],
-  },
-  chatView: {
-    flex: 13,
-  },
-  chatContainer: {
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    paddingTop: 8,
-  },
-  loadMoreSpinnerView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingBottom: 8,
-  },
-
-  spinnerView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  inputView: {
-    flex: 1,
-    padding: 12,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: theme['background-basic-color-1'],
-  },
-  input: {
-    flex: 1,
-    fontSize: theme['text-primary-size'],
-    color: theme['text-basic-color'],
-    margin: 0,
-    marginHorizontal: 4,
-    paddingTop: 4,
-    paddingLeft: 4,
-    paddingRight: 4,
-    paddingBottom: 4,
-    height: 48,
-  },
-  addMessageButton: {
-    paddingHorizontal: 0,
-    backgroundColor: 'transparent',
-  },
-  backdrop: {
-    backgroundColor: theme['back-drop-color'],
   },
 });
 

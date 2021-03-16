f4a29dfd43ee7aeccad84e79961039e3c145fbbc
Muhsin <muhsinkeramam@gmail.com>
2021-03-16 13:53:19 +0530

    update sentry config

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import { AppRegistry } from 'react-native';
 import * as Sentry from '@sentry/react-native';
-
+import Config from 'react-native-config';
 import { name as appName } from './app.json';
-
-import { SENTRY_TOKEN_URL } from './sentry';
 
 import App from './src/app';
 
 AppRegistry.registerComponent(appName, () => App);
-
 Sentry.init({
-  dsn: SENTRY_TOKEN_URL,
+  dsn: Config.SENTRY_DSN,
 });

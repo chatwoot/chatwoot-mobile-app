67240849403510b750a3e8d76dc79dd15407a89a
Muhsin <muhsinkeramam@gmail.com>
2021-03-22 19:47:58 +0530

    add chat-us via chatwoot -rn-widget

--- src/screens/Settings/components/SettingsItem.js
+++ src/screens/Settings/components/SettingsItem.js
@@ -3,7 +3,7 @@ import { TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import { Toggle, Divider, Icon, withStyles } from '@ui-kitten/components';
 
-import CustomText from './Text';
+import CustomText from 'components/Text';
 
 const styles = (theme) => ({
   section: {

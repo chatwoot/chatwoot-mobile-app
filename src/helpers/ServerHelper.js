9dd52c6003e4be99ee67afb229c4718e520e6591
Muhsin <muhsinkeramam@gmail.com>
2021-05-18 17:14:37 +0530

    update message

--- src/helpers/ServerHelper.js
+++ src/helpers/ServerHelper.js
@@ -1,12 +1,16 @@
 import Config from 'react-native-config';
-
+import { Alert } from 'react-native';
 export function checkServerSupport({ installedVersion, userRole }) {
   const minimumVersion = Config.MINIMUM_CHATWOOT_VERSION;
+  const title = 'Server upgrade required';
+  let message = `It seems like  the chatwoot server version is unsupported for this app. Upgrading to server version ${minimumVersion} or later is required`;
   if (installedVersion < minimumVersion) {
     if (userRole === 'administrator') {
-      // showServerUpgradeMessageForAdmin({ message: '' });
+      Alert.alert(title, message, [{ text: 'OK' }]);
     } else {
-      // showServerUpgradeMessage({ message: '' });
+      message =
+        'It seems like the chatwoot server version is unsupported for this app. Please contact your administrator to upgrade your Chatwoot server.';
+      Alert.alert(title, message, [{ text: 'OK' }]);
     }
   }
 }

9429ef21ebcc267a1883cb47e47265b6a8a78d57
Muhsin <muhsinkeramam@gmail.com>
2022-10-31 13:02:51 +0530

    Add storybook
    - Added @storybook/react-native.
    - Added @storybook/react-native-server.
    - Added & configured react-native-storybook-loader.
    - Added stories for `Text` component.

---
+++ storybook/storyLoader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/Text/Text.stories');
+}
+
+const stories = ['../src/components/Text/Text.stories'];
+
+module.exports = {
+  loadStories,
+  stories,
+};

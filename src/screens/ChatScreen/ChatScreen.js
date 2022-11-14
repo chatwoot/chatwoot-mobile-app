9f7938228f316b724209d3ced0a93af9e219d157
Muhsin <muhsinkeramam@gmail.com>
2022-11-14 12:00:50 +0530

    Change chat screen to functional component

--- src/screens/ChatScreen/ChatScreen.js
+++ src/screens/ChatScreen/ChatScreen.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Spinner, withStyles } from '@ui-kitten/components';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { View, SafeAreaView, SectionList } from 'react-native';
 import ChatMessage from './components/ChatMessage';
@@ -13,10 +13,8 @@ import { openURL } from '../../helpers/UrlHelper';
 
 import {
   loadMessages,
-  sendMessage,
   markMessagesAsRead,
   resetConversation,
-  toggleTypingStatus,
   getConversationDetails,
 } from '../../actions/conversation';
 
@@ -24,68 +22,56 @@ import { markNotificationAsRead } from '../../actions/notification';
 import { getGroupedConversation, findUniqueMessages } from '../../helpers';
 import { actions as CannedResponseActions } from '../../reducer/cannedResponseSlice';
 
-class ChatScreenComponent extends Component {
-  static propTypes = {
-    eva: PropTypes.shape({
-      style: PropTypes.object,
-      theme: PropTypes.object,
-    }).isRequired,
-    route: PropTypes.object,
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func.isRequired,
-      goBack: PropTypes.func.isRequired,
-    }).isRequired,
-    resetConversation: PropTypes.func,
-    cannedResponses: PropTypes.array.isRequired,
-    allMessages: PropTypes.array.isRequired,
-    conversationDetails: PropTypes.object,
-    sendMessage: PropTypes.func,
-    loadMessages: PropTypes.func,
-    fetchCannedResponses: PropTypes.func,
-    isFetching: PropTypes.bool,
-    isAllMessagesLoaded: PropTypes.bool,
-    markAllMessagesAsRead: PropTypes.func,
-    toggleTypingStatus: PropTypes.func,
-    markMessagesAsRead: PropTypes.func,
-    markNotificationAsRead: PropTypes.func,
-    getConversationDetails: PropTypes.func,
-    toggleConversationStatus: PropTypes.func,
-    conversationTypingUsers: PropTypes.shape({}),
-  };
+const propTypes = {
+  eva: PropTypes.shape({
+    style: PropTypes.object,
+    theme: PropTypes.object,
+  }).isRequired,
+  route: PropTypes.object,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+  resetConversation: PropTypes.func,
+  cannedResponses: PropTypes.array.isRequired,
+  allMessages: PropTypes.array.isRequired,
+  conversationDetails: PropTypes.object,
+  loadMessages: PropTypes.func,
+  fetchCannedResponses: PropTypes.func,
+  isFetching: PropTypes.bool,
+  isAllMessagesLoaded: PropTypes.bool,
+  markAllMessagesAsRead: PropTypes.func,
+  toggleTypingStatus: PropTypes.func,
+  markMessagesAsRead: PropTypes.func,
+  markNotificationAsRead: PropTypes.func,
+  getConversationDetails: PropTypes.func,
+  conversationTypingUsers: PropTypes.shape({}),
+};
 
-  static defaultProps = {
-    isFetching: false,
-    isAllMessagesLoaded: false,
-    sendMessage: () => {},
-    markMessagesAsRead: () => {},
-    allMessages: [],
-    cannedResponses: [],
-    conversationTypingUsers: {},
-  };
+const defaultProps = {
+  isFetching: false,
+  isAllMessagesLoaded: false,
+  markMessagesAsRead: () => {},
+  allMessages: [],
+  cannedResponses: [],
+  conversationTypingUsers: {},
+};
 
-  state = {
-    message: '',
-    onEndReachedCalledDuringMomentum: true,
-    menuVisible: false,
-    selectedIndex: null,
-    showScrollToButton: false,
-    conversationStatus: null,
-  };
+const ChatScreenComponent = ({ eva: { style }, navigation, route }) => {
+  const dispatch = useDispatch();
+  const [onEndReachedCalledDuringMomentum, setOnEndReachedCalledDuringMomentum] = useState(true);
+  const conversationDetails = useSelector(state => state.conversation.conversationDetails);
+  const allMessages = useSelector(state => state.conversation.allMessages);
+  const isFetching = useSelector(state => state.conversation.isFetching);
+  const isAllMessagesLoaded = useSelector(state => state.conversation.isAllMessagesLoaded);
+  const conversationTypingUsers = useSelector(state => state.conversation.conversationTypingUsers);
+  const showScrollToButton = false;
 
-  componentDidMount = () => {
-    const { markAllMessagesAsRead, route } = this.props;
+  useEffect(() => {
     const { conversationId, meta, messages, primaryActorDetails } = route.params;
 
-    // Clear all notification related the conversation
-    if (primaryActorDetails) {
-      this.props.markNotificationAsRead({
-        primaryActorId: primaryActorDetails.primary_actor_id,
-        primaryActorType: primaryActorDetails.primary_actor_type,
-      });
-    }
-    // Reset all messages if app is opening from external link (Deep linking or Push)
     if (!meta) {
-      this.props.resetConversation();
+      dispatch(resetConversation());
     }
     let beforeId = null;
     if (messages && messages.length) {
@@ -93,15 +79,21 @@ class ChatScreenComponent extends Component {
       const { id } = lastMessage;
       beforeId = id;
     }
-    this.props.loadMessages({ conversationId, beforeId });
-    this.props.getConversationDetails({ conversationId });
-    this.props.markMessagesAsRead({ conversationId });
-    this.props.fetchCannedResponses();
-    markAllMessagesAsRead({ conversationId });
-  };
+    dispatch(loadMessages({ conversationId, beforeId }));
+    dispatch(getConversationDetails({ conversationId }));
+    dispatch(markMessagesAsRead({ conversationId }));
+    dispatch(CannedResponseActions.index());
+    if (primaryActorDetails && primaryActorDetails.primary_actor_id) {
+      dispatch(
+        markNotificationAsRead({
+          primaryActorId: primaryActorDetails.primary_actor_id,
+          primaryActorType: primaryActorDetails.primary_actor_type,
+        }),
+      );
+    }
+  }, [dispatch, route.params]);
 
-  showAttachment = ({ type, dataUrl }) => {
-    const { navigation } = this.props;
+  const showAttachment = ({ type, dataUrl }) => {
     if (type === 'image') {
       navigation.navigate('ImageScreen', {
         imageUrl: dataUrl,
@@ -111,37 +103,25 @@ class ChatScreenComponent extends Component {
     }
   };
 
-  onBackPress = () => {
-    const { navigation } = this.props;
+  const onBackPress = () => {
     navigation.goBack();
   };
-  loadMoreMessages = () => {
-    const { allMessages, isAllMessagesLoaded } = this.props;
-
+  const loadMoreMessages = () => {
     if (!isAllMessagesLoaded) {
       const [lastMessage] = allMessages;
       const { conversation_id: conversationId, id: beforeId } = lastMessage;
-      this.props.loadMessages({ conversationId, beforeId });
+      dispatch(loadMessages({ conversationId, beforeId }));
     }
   };
 
-  onEndReached = ({ distanceFromEnd }) => {
-    const { onEndReachedCalledDuringMomentum } = this.state;
+  const onEndReached = ({ distanceFromEnd }) => {
     if (!onEndReachedCalledDuringMomentum) {
-      this.loadMoreMessages();
-      this.setState({
-        onEndReachedCalledDuringMomentum: true,
-      });
+      loadMoreMessages();
+      setOnEndReachedCalledDuringMomentum(true);
     }
   };
 
-  renderMoreLoader = () => {
-    const {
-      isAllMessagesLoaded,
-      isFetching,
-      eva: { style },
-    } = this.props;
-
+  const renderMoreLoader = () => {
     return (
       <View style={style.loadMoreSpinnerView}>
         {!isAllMessagesLoaded && isFetching ? <Spinner size="medium" color="red" /> : null}
@@ -149,139 +129,95 @@ class ChatScreenComponent extends Component {
     );
   };
 
-  renderMessage = item => (
-    <ChatMessage message={item.item} key={item.index} showAttachment={this.showAttachment} />
+  const renderMessage = item => (
+    <ChatMessage message={item.item} key={item.index} showAttachment={showAttachment} />
   );
 
-  scrollToBottom = () => {
-    this.setState({
-      showScrollToButton: false,
-    });
-    this.SectionListReference.scrollToLocation({
-      animated: true,
-      itemIndex: 0,
-      viewPosition: 0,
-    });
+  const scrollToBottom = () => {
+    // this.setState({
+    //   showScrollToButton: false,
+    // });
+    // this.SectionListReference.scrollToLocation({
+    //   animated: true,
+    //   itemIndex: 0,
+    //   viewPosition: 0,
+    // });
   };
 
-  setCurrentReadOffset(event) {
-    const scrollHight = Math.floor(event.nativeEvent.contentOffset.y);
+  const setCurrentReadOffset = event => {
+    // const scrollHight = Math.floor(event.nativeEvent.contentOffset.y);
+    // if (scrollHight > 50) {
+    //   this.setState({
+    //     showScrollToButton: false,
+    //   });
+    // } else {
+    //   this.setState({
+    //     showScrollToButton: false,
+    //   });
+    // }
+  };
 
-    if (scrollHight > 50) {
-      this.setState({
-        showScrollToButton: false,
-      });
-    } else {
-      this.setState({
-        showScrollToButton: false,
-      });
-    }
-  }
-
-  showConversationDetails = () => {
-    const { conversationDetails, navigation } = this.props;
-
+  const showConversationDetails = () => {
     if (conversationDetails) {
       navigation.navigate('ConversationDetails', { conversationDetails });
     }
   };
 
-  render() {
-    const {
-      allMessages,
-      isFetching,
-      eva: { style },
-      route,
-      conversationTypingUsers,
-      conversationDetails,
-    } = this.props;
-    const {
-      params: { conversationId, meta },
-    } = route;
+  const {
+    params: { conversationId, meta },
+  } = route;
 
-    const { showScrollToButton } = this.state;
-    const uniqueMessages = findUniqueMessages({ allMessages });
-    const groupedConversationList = getGroupedConversation({
-      conversations: uniqueMessages,
-    });
+  const uniqueMessages = findUniqueMessages({ allMessages });
+  const groupedConversationList = getGroupedConversation({
+    conversations: uniqueMessages,
+  });
 
-    return (
-      <SafeAreaView style={style.mainContainer}>
-        <ChatHeader
-          conversationId={conversationId}
-          conversationTypingUsers={conversationTypingUsers}
-          conversationDetails={conversationDetails}
-          conversationMetaDetails={meta}
-          showConversationDetails={this.showConversationDetails}
-          onBackPress={this.onBackPress}
-        />
+  return (
+    <SafeAreaView style={style.mainContainer}>
+      <ChatHeader
+        conversationId={conversationId}
+        conversationTypingUsers={conversationTypingUsers}
+        conversationDetails={conversationDetails}
+        conversationMetaDetails={meta}
+        showConversationDetails={showConversationDetails}
+        onBackPress={onBackPress}
+      />
 
-        <View style={style.container} autoDismiss={false}>
-          <View style={style.chatView}>
-            {groupedConversationList.length ? (
-              <SectionList
-                keyboardShouldPersistTaps="never"
-                scrollEventThrottle={16}
-                onScroll={event => this.setCurrentReadOffset(event)}
-                ref={ref => {
-                  this.SectionListReference = ref;
-                }}
-                inverted
-                onEndReached={this.onEndReached.bind(this)}
-                onEndReachedThreshold={0.5}
-                onMomentumScrollBegin={() => {
-                  this.setState({
-                    onEndReachedCalledDuringMomentum: false,
-                  });
-                }}
-                sections={groupedConversationList}
-                keyExtractor={(item, index) => item + index}
-                renderItem={this.renderMessage}
-                renderSectionFooter={({ section: { date } }) => <ChatMessageDate date={date} />}
-                style={style.chatContainer}
-                ListFooterComponent={this.renderMoreLoader}
-              />
-            ) : null}
-            {showScrollToButton && <ScrollToBottomButton scrollToBottom={this.scrollToBottom} />}
-            {isFetching && !groupedConversationList.length && (
-              <View style={style.loadMoreSpinnerView}>
-                <Spinner size="medium" />
-              </View>
-            )}
-          </View>
-          <ReplyBox conversationId={conversationId} conversationDetails={conversationDetails} />
+      <View style={style.container} autoDismiss={false}>
+        <View style={style.chatView}>
+          {groupedConversationList.length ? (
+            <SectionList
+              keyboardShouldPersistTaps="never"
+              scrollEventThrottle={16}
+              onScroll={event => setCurrentReadOffset(event)}
+              inverted
+              onEndReached={onEndReached.bind(this)}
+              onEndReachedThreshold={0.5}
+              onMomentumScrollBegin={() => {
+                setOnEndReachedCalledDuringMomentum(false);
+              }}
+              sections={groupedConversationList}
+              keyExtractor={(item, index) => item + index}
+              renderItem={renderMessage}
+              renderSectionFooter={({ section: { date } }) => <ChatMessageDate date={date} />}
+              style={style.chatContainer}
+              ListFooterComponent={renderMoreLoader}
+            />
+          ) : null}
+          {showScrollToButton && <ScrollToBottomButton scrollToBottom={scrollToBottom} />}
+          {isFetching && !groupedConversationList.length && (
+            <View style={style.loadMoreSpinnerView}>
+              <Spinner size="medium" />
+            </View>
+          )}
         </View>
-      </SafeAreaView>
-    );
-  }
-}
+        <ReplyBox conversationId={conversationId} conversationDetails={conversationDetails} />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-function bindAction(dispatch) {
-  return {
-    resetConversation: () => dispatch(resetConversation()),
-    loadMessages: ({ conversationId, beforeId }) =>
-      dispatch(loadMessages({ conversationId, beforeId })),
-    fetchCannedResponses: () => dispatch(CannedResponseActions.index()),
-    getConversationDetails: ({ conversationId }) =>
-      dispatch(getConversationDetails({ conversationId })),
-    sendMessage: ({ conversationId, message }) =>
-      dispatch(sendMessage({ conversationId, message })),
-    markAllMessagesAsRead: ({ conversationId }) => dispatch(markMessagesAsRead({ conversationId })),
-    toggleTypingStatus: ({ conversationId, typingStatus }) =>
-      dispatch(toggleTypingStatus({ conversationId, typingStatus })),
-    markNotificationAsRead: ({ primaryActorId, primaryActorType }) =>
-      dispatch(markNotificationAsRead({ primaryActorId, primaryActorType })),
-  };
-}
-function mapStateToProps(state) {
-  return {
-    conversationDetails: state.conversation.conversationDetails,
-    allMessages: state.conversation.allMessages,
-    isFetching: state.conversation.isFetching,
-    isAllMessagesLoaded: state.conversation.isAllMessagesLoaded,
-    conversationTypingUsers: state.conversation.conversationTypingUsers,
-  };
-}
-
+ChatScreenComponent.propTypes = propTypes;
+ChatScreenComponent.defaultProps = defaultProps;
 const ChatScreen = withStyles(ChatScreenComponent, styles);
-export default connect(mapStateToProps, bindAction)(ChatScreen);
+export default ChatScreen;

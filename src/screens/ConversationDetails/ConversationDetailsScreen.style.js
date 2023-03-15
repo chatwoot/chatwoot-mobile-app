c7a91b7d3a19e1d2cfa025865b5260cdb9d901da
iamsivin <iamsivin@gmail.com>
2023-03-15 12:57:55 +0530

    chore: Minor fixes

--- src/screens/ConversationDetails/ConversationDetailsScreen.style.js
+++ src/screens/ConversationDetails/ConversationDetailsScreen.style.js
@@ -8,41 +8,35 @@ export default theme => {
       flex: 1,
       backgroundColor: colors.background,
     },
-
     avatarContainer: {
       paddingTop: spacing.smaller,
       paddingBottom: spacing.small,
     },
-
     detailsWrap: {
       paddingBottom: spacing.small,
       paddingLeft: spacing.small,
       paddingRight: spacing.small,
     },
-
     descriptionContainer: {
       paddingVertical: spacing.micro,
     },
-
     description: {
       lineHeight: 20,
     },
-
     socialIconsContainer: {
       flexDirection: 'row',
     },
-
     separator: {
       backgroundColor: colors.backgroundLight,
+      borderWidth: 0.4,
+      borderColor: colors.borderLight,
       width: '100%',
       paddingVertical: spacing.smaller,
       paddingLeft: spacing.small,
     },
-
     separatorView: {
       width: '100%',
     },
-
     accordionItemWrapper: {
       flexDirection: 'column',
       paddingTop: spacing.small,

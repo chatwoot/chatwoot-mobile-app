3c06ebec2f4dd0286523c90c22583eeb7f74f81f
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-10-29 12:37:16 +0530

    chore: Improve server upgrade banner (#827)
    * chore: improve server upgrade logic
    
    * chore: add package
    
    * chore: add specs
    
    * chore: update `MINIMUM_CHATWOOT_VERSION`

--- src/helpers/ServerHelper.js
+++ src/helpers/ServerHelper.js
@@ -1,9 +1,20 @@
 import { Alert } from 'react-native';
-import I18n from '../i18n';
+import semver from 'semver';
+
+import I18n from '@/i18n';
+
+export const checkShouldShowServerUpgradeWarning = ({ installedVersion, minimumVersion }) => {
+  try {
+    return semver.lt(installedVersion, minimumVersion);
+  } catch (error) {
+    return false;
+  }
+};
 
 const minimumVersion = process.env.EXPO_PUBLIC_MINIMUM_CHATWOOT_VERSION;
 export function checkServerSupport({ installedVersion, userRole }) {
-  if (installedVersion < minimumVersion) {
+  const shouldShowServerUpgradeWarning = semver.lt(installedVersion, minimumVersion);
+  if (shouldShowServerUpgradeWarning) {
     if (userRole === 'administrator') {
       Alert.alert(
         I18n.t('SERVER_UPGRADE.TITLE'),

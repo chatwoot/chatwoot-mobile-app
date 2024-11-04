45ea3ba14ecb3de67e5017a6b3855f075d9d64b1
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-11-05 00:04:19 +0530

    chore: remove unused constants

--- src/screens/settings/SettingsHeader.tsx
+++ src/screens/settings/SettingsHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Animated from 'react-native-reanimated';
-import i18n from 'i18n';
 
+import i18n from 'i18n';
 import { tailwind } from '@/theme';
 
 export const SettingsHeader = () => {

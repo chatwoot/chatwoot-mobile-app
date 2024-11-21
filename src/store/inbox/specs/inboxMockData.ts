1856d0ea3f2614ada48769d49e908974a7ca12ca
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-11-21 14:21:43 +0530

    Create inboxMockData.ts

---
+++ src/store/inbox/specs/inboxMockData.ts
@@ -0,0 +1,11 @@
+import type { Inbox } from '@/types/Inbox';
+
+export const inbox: Inbox = {
+  id: 1,
+  avatarUrl: 'https://example.com/avatar.png',
+  channelId: 1,
+  name: 'Test Inbox',
+  channelType: 'Channel::WebWidget',
+  phoneNumber: '+1234567890',
+  medium: 'web',
+};

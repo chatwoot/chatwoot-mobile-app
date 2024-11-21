20a5716c7761ffbad10049caaa258b23b6042941
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-11-21 14:32:44 +0530

    chore: add label service spec

---
+++ src/store/label/specs/labelMockData.ts
@@ -0,0 +1,13 @@
+import type { Label } from '@/types';
+
+export const label1: Label = {
+  id: 1,
+  title: 'bug',
+  color: '#ff0000',
+  description: 'This is a bug label',
+  showOnSidebar: true,
+};
+
+export const mockLabelsResponse = {
+  data: [label1],
+};

5bedc0067735484421386ba4fe88c231d9974c31
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-11-26 23:21:48 +0530

    feat: search bar component

---
+++ src/components-next/common/search/SearchBar.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { SearchBar } from './SearchBar';
+import { CloseIcon } from '@/svg-icons';
+
+const meta = {
+  title: 'SearchBar',
+  component: SearchBar,
+} satisfies Meta<typeof SearchBar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+// Search with loading
+export const WithLoading: Story = {
+  args: {
+    isLoading: true,
+  },
+};
+
+// Search with prefix icon
+export const WithPrefixIcon: Story = {
+  args: {
+    prefix: <CloseIcon />,
+  },
+};

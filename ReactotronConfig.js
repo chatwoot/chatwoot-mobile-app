a3fa563096fd361bf5a2496dfc4af8cecc36802d
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-11-28 12:35:15 +0530

    chore: minor updates

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -3,7 +3,7 @@ import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 
 const reactotron = Reactotron.useReactNative() // add all built-in react native plugins
-  .use(reactotronRedux())
+  // .use(reactotronRedux())
   .connect(); //Don't forget about me!
 
 export default reactotron;

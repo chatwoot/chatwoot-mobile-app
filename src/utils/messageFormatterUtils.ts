b3f4c61c1a5dbe935f3938a370deef4e6e1a9103
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-11-28 11:24:54 +0530

    chore: fix markdown

--- src/utils/messageFormatterUtils.ts
+++ src/utils/messageFormatterUtils.ts
@@ -1,17 +1,9 @@
-// TODO: Please change this with better implementation, https://github.com/chatwoot/chatwoot/blob/develop/app/javascript/shared/composables/useMessageFormatter.js#L33
+import markdownToTxt from '@chatwoot/markdown-to-txt';
 
 export const getPlainText = (message: string) => {
   try {
-    // eslint-disable-next-line no-useless-escape
-    const regex = /\[@([^\]]+)\]\(mention:\/\/user\/\d+\/([^\)]+)\)/g;
-    const matches = message.matchAll(regex);
-    let result = message;
-    for (const match of matches) {
-      const [fullMatch, username] = match;
-      const replacement = `@${decodeURIComponent(username)}`;
-      result = result.replace(fullMatch, replacement);
-    }
-    return result;
+    const lastMessageContent = message ? markdownToTxt(message) : '';
+    return lastMessageContent;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (e) {
     return message;

b801d38b2c0652aefc2704e0ee61592bb7a448e8
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-11-29 15:47:33 +0530

    chore: add missing packages

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,6 +1,9 @@
 import Reactotron from 'reactotron-react-native';
+// Don't remove this import, this is used by reactotron-redux only in dev mode
+import { reactotronRedux } from 'reactotron-redux';
 
 const reactotron = Reactotron.useReactNative() // add all built-in react native plugins
+  .use(reactotronRedux())
   .connect(); //Don't forget about me!
 
 export default reactotron;

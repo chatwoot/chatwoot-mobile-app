69915a7d39a682a7d44eeee8516cfb6430e2d1ec
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-12-05 13:12:51 +0530

    chore: remove unused files

--- src/utils/getDynamicTime.ts
+++
@@ -1,6 +0,0 @@
-import { fromUnixTime, formatDistanceToNow } from 'date-fns';
-
-export const getDynamicTime = (time: number) => {
-  const unixTime = fromUnixTime(time);
-  return formatDistanceToNow(unixTime, { addSuffix: true });
-};

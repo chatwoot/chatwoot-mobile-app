f3823163c96b23d234567f23300b803961cf3fcc
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-12-05 13:30:02 +0530

    chore: add missing files

--- src/navigation/tabs/AppTabs.tsx
+++ src/navigation/tabs/AppTabs.tsx
@@ -20,7 +20,7 @@ import { getUserPermissions } from 'helpers/permissionHelper';
 import { CONVERSATION_PERMISSIONS } from 'constants/permissions';
 
 import { AuthStack, ConversationStack, SettingsStack, InboxStack } from '../stack';
-// import ChatScreen from '@/screens/chat-screen/ChatScreen';
+import ChatScreen from '@/screens/chat-screen/ChatScreen';
 // import ContactDetailsScreen from '@/screens/contact-details/ContactDetailsScreen';
 // import { DashboardScreen } from '@/screens/dashboard';
 
@@ -166,12 +166,12 @@ export const AppTabs = () => {
     return (
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Tab" component={Tabs} />
-        {/* <Stack.Screen
+        <Stack.Screen
           options={{ animation: 'slide_from_right' }}
           name="ChatScreen"
           component={ChatScreen}
         />
-        <Stack.Screen
+        {/*  <Stack.Screen
           options={{
             presentation: 'formSheet',
             animation: 'slide_from_bottom',

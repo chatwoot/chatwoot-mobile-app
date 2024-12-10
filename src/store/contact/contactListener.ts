2dfeb109eaa74136d61cf85fa52ca84bbe0723ee
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-12-10 23:45:29 +0530

    feat: add contacts on new notification

--- src/store/contact/contactListener.ts
+++ src/store/contact/contactListener.ts
@@ -2,8 +2,11 @@
 
 import { createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
 import { conversationActions } from '../conversation/conversationActions';
+import { notificationActions } from '../notification/notificationAction';
+import { addNotification } from '../notification/notificationSlice';
 import { addContact, addContacts } from './contactSlice';
 import { Conversation } from '@/types/Conversation';
+import { Notification } from '@/types/Notification';
 
 export const contactListenerMiddleware = createListenerMiddleware();
 
@@ -24,6 +27,35 @@ contactListenerMiddleware.startListening({
   effect: (action, listenerApi) => {
     const conversation = action.payload as Conversation;
     const contact = conversation?.meta?.sender;
+    if (contact) {
+      listenerApi.dispatch(addContact(contact));
+    }
+  },
+});
+
+contactListenerMiddleware.startListening({
+  matcher: isAnyOf(notificationActions.fetchNotifications.fulfilled),
+  effect: (action, listenerApi) => {
+    const { payload: notifications } = action.payload;
+    const conversationNotifications = notifications.filter(
+      (notification: Notification) =>
+        notification.primaryActorType === 'Conversation' && notification.primaryActor?.meta?.sender,
+    );
+    const contacts = conversationNotifications.map(
+      (notification: Notification) => notification.primaryActor?.meta?.sender,
+    );
+    if (contacts.length > 0) {
+      listenerApi.dispatch(addContacts({ contacts }));
+    }
+  },
+});
+
+contactListenerMiddleware.startListening({
+  matcher: isAnyOf(addNotification),
+  effect: (action, listenerApi) => {
+    const { payload } = action;
+    const { notification } = payload;
+    const contact = notification?.primaryActor?.meta?.sender;
     if (contact) {
       listenerApi.dispatch(addContact(contact));
     }

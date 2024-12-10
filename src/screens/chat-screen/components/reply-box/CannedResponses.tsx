4b99218883a1f26ef528529dbd2ada7a0598c4e7
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-12-10 23:23:10 +0530

    feat: complete canned responses

--- src/screens/chat-screen/components/reply-box/CannedResponses.tsx
+++ src/screens/chat-screen/components/reply-box/CannedResponses.tsx
@@ -7,10 +7,34 @@ import { selectAllCannedResponses } from '@/store/canned-response/cannedResponse
 import { useAppDispatch, useAppSelector } from '@/hooks';
 import { cannedResponseActions } from '@/store/canned-response/cannedResponseActions';
 import { CannedResponse } from '@/types';
+import { FlashList } from '@shopify/flash-list';
 
 type CannedResponsesProps = {
   searchKey: string;
   onSelect: (cannedResponse: CannedResponse) => void;
+};
+
+const CannedResponseItem = ({
+  item,
+  onSelect,
+}: {
+  item: CannedResponse;
+  onSelect: (cannedResponse: CannedResponse) => void;
+}) => {
+  return (
+    <Pressable
+      onPress={() => onSelect(item)}
+      style={tailwind.style(
+        'w-full flex-row justify-between items-center border-b border-gray-200 py-3 px-4',
+      )}>
+      <Animated.Text numberOfLines={1} style={tailwind.style('text-md flex-1 text-gray-950')}>
+        {item.content.replace(/\n/g, ' ')}
+      </Animated.Text>
+      <Animated.Text style={tailwind.style('text-sm text-gray-900 ml-2')}>
+        {`/${item.shortCode}`}
+      </Animated.Text>
+    </Pressable>
+  );
 };
 
 export const CannedResponses = (props: CannedResponsesProps) => {
@@ -18,7 +42,8 @@ export const CannedResponses = (props: CannedResponsesProps) => {
   const cannedResponses = useAppSelector(selectAllCannedResponses);
 
   useEffect(() => {
-    dispatch(cannedResponseActions.index({ searchKey: props.searchKey }));
+    const searchKey = props.searchKey.slice(1);
+    dispatch(cannedResponseActions.index({ searchKey }));
   }, [props.searchKey]);
 
   if (!props.searchKey || cannedResponses.length === 0) {
@@ -32,22 +57,14 @@ export const CannedResponses = (props: CannedResponsesProps) => {
           'absolute bottom-full left-0 right-0 bg-white border-t border-gray-200  max-h-[180px]',
         ),
       ]}>
-      <ScrollView>
-        {cannedResponses.map(cannedResponse => (
-          <Pressable
-            onPress={() => props.onSelect(cannedResponse)}
-            style={tailwind.style(
-              'w-full flex-row justify-between items-center border-b border-gray-200 py-3 px-4',
-            )}
-            key={cannedResponse.id}>
-            <Animated.Text numberOfLines={1} style={tailwind.style('text-md flex-1 text-gray-950')}>
-              {cannedResponse.content.replace(/\n/g, ' ')}
-            </Animated.Text>
-            <Animated.Text style={tailwind.style('text-sm text-gray-900 ml-2')}>
-              {`/${cannedResponse.shortCode}`}
-            </Animated.Text>
-          </Pressable>
-        ))}
+      <ScrollView keyboardShouldPersistTaps="always">
+        <FlashList
+          data={cannedResponses}
+          renderItem={({ item }) => <CannedResponseItem item={item} onSelect={props.onSelect} />}
+          estimatedItemSize={50}
+          keyExtractor={item => item.id.toString()}
+          keyboardShouldPersistTaps="always"
+        />
       </ScrollView>
     </Animated.View>
   );

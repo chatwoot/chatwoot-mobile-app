a8d0183433f396aa7f675764d6db5ca5c8fe1823
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-03-18 11:40:36 +0530

    chore: fix translation issues

--- src/components-next/common/filters/FilterBar.tsx
+++ src/components-next/common/filters/FilterBar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Animated, { LinearTransition, withTiming } from 'react-native-reanimated';
 import { tailwind } from '@/theme';
 import { FilterButton } from './FilterButton';
+import i18n from '@/i18n';
 
 // Generic type for filter options
 export type BaseFilterOption = {
@@ -32,22 +33,42 @@ export const FilterBar = ({ allFilters, selectedFilters, onFilterPress }: Filter
     };
   };
 
+  const getFilterTitle = (value: BaseFilterOption) => {
+    return i18n.t(
+      `CONVERSATION.FILTERS.${value.type.toUpperCase()}.OPTIONS.${selectedFilters[value.type].toUpperCase()}`,
+    );
+  };
+
   return (
     <Animated.View
       exiting={exiting}
       style={tailwind.style('px-3 pt-2 pb-1.5 h-[46px] flex flex-row')}>
-      {allFilters.map((value, index) => (
-        <Animated.View
-          layout={LinearTransition.springify().stiffness(200).damping(24)}
-          key={index}
-          style={tailwind.style('pr-2')}>
-          <FilterButton
-            handleOnPress={() => onFilterPress(value.type)}
-            allFilters={value}
-            selectedFilters={selectedFilters}
-          />
-        </Animated.View>
-      ))}
+      {allFilters.map((value, index) => {
+        if (value.type === 'inbox_id') {
+          return (
+            <Animated.View
+              layout={LinearTransition.springify().stiffness(200).damping(24)}
+              key={index}
+              style={tailwind.style('pr-2')}>
+              <FilterButton
+                handleOnPress={() => onFilterPress(value.type)}
+                value={value.options[selectedFilters[value.type] as keyof typeof value.options]}
+              />
+            </Animated.View>
+          );
+        }
+        return (
+          <Animated.View
+            layout={LinearTransition.springify().stiffness(200).damping(24)}
+            key={index}
+            style={tailwind.style('pr-2')}>
+            <FilterButton
+              handleOnPress={() => onFilterPress(value.type)}
+              value={getFilterTitle(value)}
+            />
+          </Animated.View>
+        );
+      })}
     </Animated.View>
   );
 };

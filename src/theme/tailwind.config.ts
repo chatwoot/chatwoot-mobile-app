e86b7f7114bdc51c783a932e5d46fdb72675a2f9
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-04-12 13:05:42 +0530

    chore: add more doc

--- src/theme/tailwind.config.ts
+++ src/theme/tailwind.config.ts
@@ -1,10 +1,14 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+// Light theme colors
 const radixUILightColors = require('./colors/light');
+// Dark theme colors
 const radixUIDarkColors = require('./colors/dark');
 
+// Black with alpha variations
 const blackA = require('./colors/blackA');
+// White with alpha variations
 const whiteA = require('./colors/whiteA');
 
 const chatwootAppColors = {
@@ -24,6 +28,24 @@ export const twConfig = {
         cxs: '13px',
         md: '15px',
       },
+      cursor: {
+        default: 'default',
+        pointer: 'pointer',
+        text: 'text',
+        'not-allowed': 'not-allowed',
+        wait: 'wait',
+        move: 'move',
+      },
+      // We are using individual static Inter font files (e.g., Inter-400-20.ttf) to load different font weights and styles
+      // Please refer to the following links for more information:
+      // https://medium.com/timeless/adding-custom-variable-fonts-in-react-native-47e0d062bcfc
+      // https://medium.com/timeless/adding-custom-variable-fonts-in-react-native-part-ii-d11a979a38f3
+      // - Normal/Regular: "inter-normal-20" (400)
+      // - Slightly heavier than Regular: "inter-420-20" (400)
+      // - Medium: "inter-medium-24" (500)
+      // - Between Medium and Semi-bold: "inter-580-24" (600)
+      // - Semi-bold: "inter-semibold-24" (600)
+      // -  Last numbers (20, 24 etc) are optical sizes.
       fontFamily: {
         'inter-normal-20': ['Inter-400-20'],
         'inter-420-20': ['Inter-420-20'],

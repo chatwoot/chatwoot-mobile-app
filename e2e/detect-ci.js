aca432c81c9f5a4c27cb2b9914d98810bc48afbb
Bastin <BastinJafari@gmail.com>
2025-04-12 08:11:37 +0200

    change detect ci

--- e2e/detect-ci.js
+++ e2e/detect-ci.js
@@ -15,9 +15,8 @@ function getTestConfig() {
   // Default configuration
   const config = {
     inCI,
-    // In CI environments, we'll use Safari/Chrome for web testing
-    // instead of trying to build the app
-    useWebApp: inCI || false,
+    // Always use app testing, never web
+    useWebApp: false,
     waitTimeMultiplier: inCI ? 2 : 1, // Increase wait times in CI
   };
   

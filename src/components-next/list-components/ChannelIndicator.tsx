f57e897459f3bfb5dc7f2a6a075cd85ad36d7ecd
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-04-12 17:05:58 +0530

    chore: small fixes

--- src/components-next/list-components/ChannelIndicator.tsx
+++ src/components-next/list-components/ChannelIndicator.tsx
@@ -18,9 +18,14 @@ export const ChannelIndicator = (props: ChannelIndicatorProps) => {
   const { type = '' } = props.additionalAttributes || {};
 
   return (
-    <NativeView style={tailwind.style('pl-1 gap-1 flex flex-row justify-center items-center')}>
+    <NativeView style={tailwind.style('pl-1 gap-1 flex flex-row justify-center items-center w-20')}>
       <Icon icon={getChannelIcon(channelType as Channel, medium, type)} size={16} />
-      <Text style={tailwind.style('text-sm font-inter-420-20 text-gray-700')}>{name}</Text>
+      <Text
+        style={tailwind.style('text-sm font-inter-420-20 text-gray-700 overflow-hidden capitalize')}
+        numberOfLines={1}
+        ellipsizeMode="tail">
+        {name}
+      </Text>
     </NativeView>
   );
 };

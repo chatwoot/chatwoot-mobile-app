39f890cd833acbe743c534f77d3ea2d8185bb0b7
Muhsin Keloth <muhsinkeramam@gmail.com>
2025-04-14 23:20:37 +0530

    feat: add typing indicator

--- src/screens/captain/TypingIndicator.tsx
+++ src/screens/captain/TypingIndicator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import { tailwind } from '@/theme';
 import { Image } from 'expo-image';
@@ -10,21 +10,15 @@ type TypingIndicatorProps = {
 
 export const TypingIndicator = ({ typingText }: TypingIndicatorProps) => {
   return (
-    <View style={tailwind.style('absolute w-full items-center -top-14')}>
+    <View style={tailwind.style('w-full items-center mb-16')}>
       <View
         style={tailwind.style(
-          'flex-row items-center py-1 px-4 shadow-md rounded-full bg-white my-1 mx-auto',
+          'flex-row items-center py-1 px-2 shadow-md rounded-xl bg-white mx-auto',
         )}>
-        <Text
-          style={tailwind.style(
-            'text-cxs font-inter-medium-24 tracking-[0.32px] leading-[18px] text-blackA-A11 text-center',
-          )}>
-          {typingText}
-        </Text>
         <Image
           // eslint-disable-next-line @typescript-eslint/no-require-imports
-          source={require('../../assets//local/typing')}
-          style={tailwind.style('w-8 h-8 ml-2')}
+          source={require('./typing.gif')}
+          style={tailwind.style('w-6 h-6')}
           contentFit="contain"
         />
       </View>

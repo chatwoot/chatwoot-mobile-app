9d7d13dbd6e8e81624f9251437b040e140694857
Phillip Mtalika <phillipmtalika1@gmail.com>
2026-01-06 05:39:52 +0200

    fix: conditional jsxImportSource for production builds

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const isDev = process.env.NODE_ENV !== 'production';
+
+  const presetOptions = isDev
+    ? { jsxImportSource: '@welldone-software/why-did-you-render' }
+    : {};
+
   return {
-    presets: [
-      [
-        'babel-preset-expo',
-        {
-          jsxImportSource: '@welldone-software/why-did-you-render',
-        },
-      ],
-    ],
+    presets: [['babel-preset-expo', presetOptions]],
     plugins: [
       [
         'module-resolver',

cde4289fb0318842e6d121e89d5ef93b9ab045bc
salmonumbrella <182032677+salmonumbrella@users.noreply.github.com>
2026-06-06 13:49:03 -0700

    fix(channels): keep tab on conversations

--- src/navigation/stack/ChannelsStack.tsx
+++ src/navigation/stack/ChannelsStack.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import ChannelsScreen from '@/screens/channels/ChannelsScreen';
 import ConversationScreen from '@/screens/conversations/ConversationScreen';
 
 export type ChannelsStackParamList = {
-  ChannelsScreen: undefined;
   ConversationScreen: { showFilters?: boolean } | undefined;
 };
 
@@ -14,11 +12,6 @@ const Stack = createNativeStackNavigator<ChannelsStackParamList>();
 const ChannelsStack = () => {
   return (
     <Stack.Navigator initialRouteName="ConversationScreen">
-      <Stack.Screen
-        options={{ headerShown: false }}
-        name="ChannelsScreen"
-        component={ChannelsScreen}
-      />
       <Stack.Screen
         options={{ headerShown: false, animation: 'slide_from_right' }}
         name="ConversationScreen"
